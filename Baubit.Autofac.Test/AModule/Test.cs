08db32ec7e41902e37164db1c479dc50918ed244
prash <prashant.nagoorkar@gmail.com>
2024-09-13 19:32:46 -0400

    Added test

--- Baubit.Autofac.Test/AModule/Test.cs
+++ Baubit.Autofac.Test/AModule/Test.cs
@@ -1,24 +1,55 @@
 ﻿using Baubit.Configuration;
 using Baubit.DI;
 using System.Reflection;
+using System.Text.Json;
 
 namespace Baubit.Autofac.Test.AModule
 {
     public class Test
     {
-        [Fact]
-        public async void ModulesCanBeLoadedDynamically()
+        [Theory]
+        [InlineData($"modules.json")]
+        public async void CanLoadModuleConfiguration_FromMetaConfiguration_UsingASingleJsonString(string jsonFile)
         {
             var readResult = await Baubit.Resource
                                          .Operations
                                          .ReadEmbeddedResource
-                                         .RunAsync(new Resource.ReadEmbeddedResource.Context($"{this.GetType().Namespace}.modules.json", Assembly.GetExecutingAssembly()));
+                                         .RunAsync(new Resource.ReadEmbeddedResource.Context($"{this.GetType().Namespace}.{jsonFile}", Assembly.GetExecutingAssembly()));
             Assert.True(readResult.Success);
             var metaConfiguration = new MetaConfiguration { RawJsonStrings = [readResult.Value] };
             var modules = metaConfiguration.Load().GetNestedModules();
             Assert.NotEmpty(modules);
             Assert.Single(modules);
             Assert.NotNull(modules.First().ModuleConfiguration);
+        }
+        [Theory]
+        [InlineData($"modules_ThreeLevelDeep.json")]
+        public async void ModulesAreSerializable(string jsonFile)
+        {
+            var readResult = await Baubit.Resource
+                                         .Operations
+                                         .ReadEmbeddedResource
+                                         .RunAsync(new Resource.ReadEmbeddedResource.Context($"{this.GetType().Namespace}.{jsonFile}", Assembly.GetExecutingAssembly()));
+
+            Assert.True(readResult.Success);
+            var metaConfiguration = new MetaConfiguration { RawJsonStrings = [readResult.Value] };
+            var modules = metaConfiguration.Load().GetNestedModules();
+
+            Assert.Equal(3, modules.Sum(m => m.CountTotalNodes()));
+            Assert.Equal(3, modules.Max(m => m.CountNodesInDeepestSubgraph()));
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            var jsonString = JsonSerializer.Serialize(new { Modules = modules }, options);
+
+            var reloadedModules = new MetaConfiguration { RawJsonStrings = [jsonString] }.Load().GetNestedModules();
+
+            Assert.Equal(3, reloadedModules.Sum(m => m.CountTotalNodes()));
+            Assert.Equal(3, reloadedModules.Max(m => m.CountNodesInDeepestSubgraph()));
+
+            var jsonString2 = JsonSerializer.Serialize(new { Modules = modules }, options);
+
+            Assert.Equal(jsonString, jsonString2);
         }
     }
 }
