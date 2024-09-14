e3140420cc820f47bb38a348a2ad24f5341b3a8b
prash <prashant.nagoorkar@gmail.com>
2024-09-14 10:11:47 -0400

    ServiceProviderMetaFactory

---
+++ Baubit.Autofac/ServiceProviderMetaFactory.cs
@@ -0,0 +1,15 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Baubit.DI;
+using Microsoft.Extensions.Hosting;
+
+namespace Baubit.Autofac
+{
+    public sealed class ServiceProviderMetaFactory : IServiceProviderMetaFactory
+    {
+        public IHostApplicationBuilder UseConfiguredServiceProviderFactory(IHostApplicationBuilder hostApplicationBuilder)
+        {
+            hostApplicationBuilder.ConfigureContainer(new AutofacServiceProviderFactory());
+            return hostApplicationBuilder;
+        }
+    }
+}
