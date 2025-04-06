3d61cfa2b74ab15e13617ff8ff802960c94288c2
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-04-06 11:21:36 -0400

    triggering build

--- Samples/ConsoleApp/MyComponent.cs
+++ Samples/ConsoleApp/MyComponent.cs
@@ -5,6 +5,7 @@ namespace ConsoleApp
     public class MyComponent
     {
         public string SomeStringValue { get; set; }
+
         public MyComponent(string someStringValue, ILogger<MyComponent> logger)
         {
             SomeStringValue = someStringValue;
