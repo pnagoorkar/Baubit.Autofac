4cf6da592a19cf4174575f3468ecc2e591e0a8f3
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-04-08 08:16:59 -0400

    RootModule diverges to maintain integrity with Baubit.Autofac.DI.AModule<>

--- Baubit.Autofac/DI/RootModule.cs
+++ Baubit.Autofac/DI/RootModule.cs
@@ -3,14 +3,50 @@ using Autofac.Builder;
 using Autofac.Extensions.DependencyInjection;
 using Baubit.Configuration;
 using Baubit.DI;
+using FluentResults;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Baubit.Autofac.DI
 {
+
+    public abstract class ARootModuleConfiguration : AConfiguration
+    {
+
+    }
     public sealed class RootModuleConfiguration : ARootModuleConfiguration
     {
         public ContainerBuildOptions ContainerBuildOptions { get; init; }
+    }
+
+    public abstract class ARootModule<TConfiguration,
+                                      TServiceProviderFactory,
+                                      TContainerBuilder> : AModule<TConfiguration>, IRootModule where TConfiguration : ARootModuleConfiguration
+                                                                                                  where TServiceProviderFactory : IServiceProviderFactory<TContainerBuilder>
+                                                                                                  where TContainerBuilder : notnull
+    {
+        protected ARootModule(ConfigurationSource configurationSource) : base(configurationSource)
+        {
+        }
+
+        protected ARootModule(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected ARootModule(TConfiguration configuration, List<AModule> nestedModules) : base(configuration, nestedModules)
+        {
+        }
+
+        public Result<THostApplicationBuilder> UseConfiguredServiceProviderFactory<THostApplicationBuilder>(THostApplicationBuilder hostApplicationBuilder) where THostApplicationBuilder : IHostApplicationBuilder
+        {
+            hostApplicationBuilder.ConfigureContainer(GetServiceProviderFactory(), GetConfigureAction());
+            return hostApplicationBuilder;
+        }
+
+        protected abstract TServiceProviderFactory GetServiceProviderFactory();
+        protected abstract Action<TContainerBuilder> GetConfigureAction();
+
     }
     public sealed class RootModule : ARootModule<RootModuleConfiguration, AutofacServiceProviderFactory, ContainerBuilder>, IModule
     {
@@ -26,7 +62,7 @@ namespace Baubit.Autofac.DI
         {
         }
 
-        public void Load(ContainerBuilder containerBuilder)
+        public override void Load(ContainerBuilder containerBuilder)
         {
             var baubitModules = new List<Baubit.DI.IModule>();
             var baubitAutofacModules = new List<IModule>();
